5e702e5ceaff770bf515a009d4d8226873acfbf2
Kawawa <denokawawa@gmail.com>
2019-07-13 20:21:36 +0300

    rothman sale

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $app->get('/api/login', 'login');
 $app->get('/api/post_sale', 'postTotalSale');
 $app->get('/api/post_stock', 'postStock');
 $app->get('/api/post_schweppes_stock', 'post_schweppes_stock');
+$app->get('/api/post_rothman_sale', 'post_rothman_sale');
 $app->post('/api/post_pic', 'postAgentPic');
 
 
@@ -209,6 +210,8 @@ function postAgentPic($request){
         echo '{"success":"false","error":"' . $e->getMessage() . '""}';
     }
 }
+
+
 function post_schweppes_sale($request){
     $sql = "insert into tbl_sales (`customer_name`,`is_aware`,`traffic_source`,`is_customer`,`purchase_source`,`stock_out_count`,`other_brand_count`,`product_description`,`product_usage`,`product_nouse`,`product_no_purchase`,`would_recommend`,`purchase_influence`,`sales_photo`,`engagement_photo`)
 values (:customer_name,:is_aware,:traffic_source,:is_customer,:purchase_source,:stock_out_count,:other_brand_count,:product_description,:product_usage,:product_nouse,:product_no_purchase,:would_recommend,:purchase_influence,:sales_photo,:engagement_photo)";
@@ -365,6 +368,98 @@ function postTotalSale($request){
  
     
 }
+
+
+
+function post_rothman_sale($request){
+    $sql = "insert into tbl_rothman_sale (`customer_name`,`is_aware`,`traffic_source`,`is_customer`,`purchase_source`,`product_description`,`customer_reaction`,`tried_variant`,`area`,`region`,`sales_photo`,`engagement_photo`)
+values (:customer_name,:is_aware,:traffic_source,:is_customer,:purchase_source,:product_description,:customer_reaction,:tried_variant,,:area,:region,:sales_photo,:engagement_photo)";
+
+    $img_path = "";
+
+    if ($_FILES['pic']['name']) {
+        $rand = rand();
+        $img_path = $rand . $_FILES['pic']['name'];
+        move_uploaded_file($_FILES["pic"]["tmp_name"], "public/".$img_path);
+    }
+
+
+    $img_path_engage = "";
+
+    if ($_FILES['pic_engage']['name']) {
+        $rand = rand();
+        $img_path_engage = $rand . $_FILES['pic_engage']['name'];
+        move_uploaded_file($_FILES["pic_engage"]["tmp_name"], "public/".$img_path_engage);
+    }
+
+
+    try {
+
+        $db = DB_Connection();
+
+        //record sale
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("customer_name",  $request->getParam('customer_name'));
+        $stmt->bindParam("is_aware",$request->getParam('is_aware') );
+        $stmt->bindParam("traffic_source", $request->getParam('traffic_source'));
+        $stmt->bindParam("is_customer",  $request->getParam('is_customer'));
+        $stmt->bindParam("purchase_source",  $request->getParam('purchase_source'));
+        $stmt->bindParam("customer_reaction",  $request->getParam('reaction'));
+        $stmt->bindParam("area",  $request->getParam('area'));
+        $stmt->bindParam("product_description",  $request->getParam('product_description'));
+        $stmt->bindParam("region",  $request->getParam('region'));
+        $stmt->bindParam("tried_variant",  $request->getParam('variant'));
+        $stmt->bindParam("sales_photo", $img_path);
+        $stmt->bindParam("engagement_photo", $img_path_engage);
+
+        $stmt->execute();
+
+        $sale_id = $db->lastInsertId();
+
+
+
+        //record sku order details
+        $skus =  json_decode($request->getParam('skus'), true);
+        $sql = "insert into tbl_orders (`sale_id`,`sku_detail`,`quantity`) values (:sale_id,:sku_detail,:quantity)";
+
+        foreach($skus as $key=>$value) {
+            $sku_detail_id = $value['sku_detail_id'];
+            $quantity = $value['quantity'];
+
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam("sale_id", $sale_id);
+            $stmt->bindParam("sku_detail", $sku_detail_id);
+            $stmt->bindParam("quantity", $quantity);
+            $stmt->execute();
+
+        }
+
+
+        //save sale detials
+        $prod_id = $request->getParam('product_id');
+        $sql ="insert into tbl_sale_details (`product_id`,`sale_id`,`agent_id`,`lat`,`lng`,`region`,`van_number`,`check_in`,`check_out`,`date_created`) values (:product_id,:sale_id,:agent_id,:lat,:lng,:region,:van_number,:check_in,:check_out,:date_created)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("product_id", $prod_id);
+        $stmt->bindParam("sale_id", $sale_id);
+        $stmt->bindParam("van_number", $request->getParam('van_number'));
+        $stmt->bindParam("agent_id", $request->getParam('agent_id'));
+        $stmt->bindParam("lat", $request->getParam('lat'));
+        $stmt->bindParam("lng", $request->getParam('lng'));
+        $stmt->bindParam("check_in", $request->getParam('check_in'));
+        $stmt->bindParam("check_out", $request->getParam('check_out'));
+        $stmt->bindParam("region", $request->getParam('region'));
+        $stmt->bindParam("date_created", date('Y-m-d H:i:s'));
+        $stmt->execute();
+
+        //update tbl_sales
+
+        echo '{"success":"true"}';
+
+    } catch (PDOException $e) {
+        echo '{"success":"false","error":"' . $e->getMessage() . '""}';
+    }
+}
+
 
 function login($request, $response, $args) {
     
